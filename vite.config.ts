247b3dd09de084fc3062514c3c94e365f5864e69
denis6421 <denis.b651@gmail.com>
2023-05-16 14:47:48 +0300

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-
+import.meta.url
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ton-vote/',
   plugins: [
     react(),
     tsconfigPaths(),
+    
   ],
   server: {
     port: 3000,
