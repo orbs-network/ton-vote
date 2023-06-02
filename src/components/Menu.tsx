03304357d7743d7646668ffa79d6127639dc4804
denis6421 <denis.b651@gmail.com>
2023-06-02 23:01:23 +0300

    a

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -20,7 +20,7 @@ export function Menu({
       PaperProps={{
         style: {
           borderRadius: 10,
-          border: theme.palette.mode === 'light' ?  "1px solid #e0e0e0" : 'unset',
+          border: theme.palette.mode === 'light' ?  "1px solid #e0e0e0" : '1px solid rgba(255,255,255, 0.2)',
           boxShadow: theme.palette.mode === 'light'  ? "rgb(114 138 150 / 8%) 0px 2px 16px" : 'unset',
         },
       }}
