d7569da36fc4e406975777b4c7058434d51d480b
denis6421 <denis.b651@gmail.com>
2023-06-05 12:10:19 +0300

    a

--- src/whitelisted.ts
+++ src/whitelisted.ts
@@ -1,8 +1,4 @@
 import { IS_DEV } from "config";
 
 export const WHITELISTED_DAOS: string[] = []
-export const WHITELISTED_PROPOSALS: string[] = IS_DEV ? [] :  [
-  "EQDMRWSwRZ4cZYHr4izVRVKY0H4CpTnAW867QTn3BdqddiR5",
-  "EQD0b665oQ8R3OpEjKToOrqQ9a9B52UnlY-VDKk73pCccvLr",
-  "EQCVy5bEWLQZrh5PYb1uP3FSO7xt4Kobyn4T9pGy2c5-i-GS",
-];
+export const WHITELISTED_PROPOSALS: string[] = []
