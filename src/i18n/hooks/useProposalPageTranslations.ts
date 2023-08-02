c492b2ab8031da5657646f4407f05c7bccca1891
Sukh Singh <sukh@orbs.com>
2023-08-02 12:02:07 +0100

    added type for proposal page translations

--- src/i18n/hooks/useProposalPageTranslations.ts
+++ src/i18n/hooks/useProposalPageTranslations.ts
@@ -1,6 +1,47 @@
 import { useTranslation } from "react-i18next";
 
-export const useProposalPageTranslations = () => {
+export type ProposalPageTranslations = {
+  castVote: string;
+  timeLeftToVote: string;
+  resultsVerified: string;
+  verifyingResults: string;
+  failedToVerifyResults: string;
+  address: string;
+  vote: string;
+  votingPower: string;
+  date: string;
+  voteStartsIn: string;
+  results: string;
+  recentVotes: string;
+  information: string;
+  startDate: string;
+  endDate: string;
+  snapshot: string;
+  contract: string;
+  votingStrategy: string;
+  votes: string;
+  showMore: string;
+  verifyInfo: string;
+  downloadCsv: string;
+  showLess: string;
+  yourVotingPower: string;
+  choice: string;
+  notEnoughVotingPower: (value: string) => string;
+  confirm: string;
+  noVotes: string;
+  you: string;
+  httpsv2Endpoint: string;
+  httpsv2ApiKey: string;
+  httpv4Endpoint: string;
+  tonAccessEnpoint: string;
+  customEndpoint: string;
+  verify: string;
+  rpcSelectTitle: string;
+  verifyResults: string;
+};
+
+
+export const useProposalPageTranslations = (): ProposalPageTranslations => {
   const { t } = useTranslation("proposalPage");
 
   return {
