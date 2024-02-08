c427bb4af1eb35dd08764476c71c55d189a44949
denis-orbs <denis@orbs.com>
2024-02-08 13:46:53 +0200

    img fix

--- src/components/Img.tsx
+++ src/components/Img.tsx
@@ -24,6 +24,7 @@ export function Img({
         <>
           <Fade in={!isLoading}>
             <StyledImg
+            referrerPolicy='no-referrer'
               onError={() => setError(true)}
               src={src}
               onLoad={() => setIsLoading(false)}
